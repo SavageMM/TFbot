0297967f73121f6d3ae81b281d4bed0590ecfbac
SavageMM <59336180+SavageMM@users.noreply.github.com>
2020-01-03 09:30:06 +0100

    int

---
+++ index.js
@@ -0,0 +1,65 @@
+const discord = require("discord.js");
+const botConfig = require("./botconfig.json");
+
+const fs = require("fs");
+
+const bot = new discord.Client();
+bot.commands = new discord.Collection();
+
+fs.readdir("./commands", (err, files) => {
+
+    if (err) console.log(err);
+
+    // filter alleen js files
+    var jsFiles = files.filter(f => f.split(".").pop() === "js");
+
+    if (jsFiles.length <= 0) {
+        HTMLFormControlsCollection.log("Can't find any file's");
+        return;
+    }
+
+    jsFiles.forEach((f, i) => {
+
+        var fileGet = require(`./commands/${f}`);
+        console.log(`${f} is loaded and ready!`);
+
+        bot.commands.set(fileGet.help.name, fileGet);
+
+    });
+
+});
+
+bot.on("ready", async () => {
+
+    console.log(`${bot.user.username} is online!`)
+
+    bot.user.setActivity("Getting Created by my dad SavageMM", { type: "WATCHING" });
+
+});
+
+bot.on("message", async message => {
+
+    if (message.author.bot) return;
+
+    if (message.channel.type === "dm") return;
+
+    var prefix = botConfig.prefix;
+
+    var messageArray = message.content.split(" ");
+
+    var command = messageArray[0];
+
+    var arguments = messageArray.slice(1);
+
+    var commands = bot.commands.get(command.slice(prefix.length));
+
+    if(commands) commands.run(bot,message, arguments);
+
+    if (command === `${prefix}hallo`) {
+
+        return message.channel.send("Hallo");
+    }
+
+});
+
+bot.login(botConfig.token);
